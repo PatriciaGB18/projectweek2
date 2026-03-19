23751cdebcf556bce0fbe0bc321f88c2859f2dff
PatriciaGB18 <pbarros@byupathway.edu>
2026-03-18 21:39:38 -0300

    Fix: Adicionando export deleteInvoice faltando

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -117,3 +117,12 @@ export async function authenticate(
     throw error;
   }
 }
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
